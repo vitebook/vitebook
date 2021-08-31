d496d0b43561f57e4e7dec202f93ff96c0ab0a77
Rahim Alwer <rahim.alwer@gmail.com>
2021-08-31 14:10:49 +1000

    chore(vitebook): setup `husky`

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 
-const packages = fs.readdirSync(path.resolve(__dirname, 'packages/@vuepress'));
+const packages = fs.readdirSync(path.resolve(__dirname, 'packages'));
 
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-enum': [2, 'always', ['vuepress', 'vuepress-vite', ...packages]]
+    'scope-enum': [2, 'always', ['vitebook', ...packages]]
   }
 };
