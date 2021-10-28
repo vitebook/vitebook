d4333fe660e72148aca2f73791399a39411883f8
Rahim Alwer <rahim.alwer@gmail.com>
2021-10-28 19:09:56 +1100

    fix(core): dev command does not print http urls for dev server

--- packages/core/src/node/cli/commands/devCommand.ts
+++ packages/core/src/node/cli/commands/devCommand.ts
@@ -1,3 +1,4 @@
+import kleur from 'kleur';
 import type { ViteDevServer } from 'vite';
 
 import { resolveApp } from '../../app/resolveApp';
@@ -6,5 +7,10 @@ import type { DevCommandArgs } from '../args';
 export async function devCommand(args: DevCommandArgs): Promise<ViteDevServer> {
   const app = await resolveApp(args);
   const server = await app.dev();
-  return server.listen();
+  console.log(kleur.cyan(`vitebook@${app.version}\n`));
+  return server.listen().then((server) => {
+    server.printUrls();
+    console.log();
+    return server;
+  });
 }
