174f1872a154ed7326bdedb787ef459396d8887e
Rahim Alwer <rahim.alwer@gmail.com>
2021-11-03 00:04:45 +1100

    fix(client): avoid crashing when no route exists

--- packages/client/src/client/components/PageView.svelte
+++ packages/client/src/client/components/PageView.svelte
@@ -2,11 +2,11 @@
   import { currentRoute } from '../stores/currentRoute';
 </script>
 
-{#if 'adapter' in $currentRoute.component}
+{#if $currentRoute && 'adapter' in $currentRoute.component}
   <svelte:component
     this={$currentRoute.component.adapter}
     component={$currentRoute.component.component}
   />
 {:else}
-  <svelte:component this={$currentRoute.component} />
+  <svelte:component this={$currentRoute?.component} />
 {/if}
