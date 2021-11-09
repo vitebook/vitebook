7e8c5b622b9629378e8b85b3607015af040c1a0b
Rahim Alwer <rahim.alwer@gmail.com>
2021-11-09 18:44:20 +1100

    chore(docs): bump `@vitebook/*` to `0.6.0`

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -6,6 +6,10 @@ const packages = fs.readdirSync(path.resolve(__dirname, 'packages'));
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-enum': [2, 'always', ['', 'release', 'vitebook', ...packages]],
+    'scope-enum': [
+      2,
+      'always',
+      ['', 'docs', 'release', 'vitebook', ...packages],
+    ],
   },
 };
