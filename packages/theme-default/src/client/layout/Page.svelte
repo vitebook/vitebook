e2f0b1ce6223e589b4394414b47bd2c80a1e69cc
Rahim Alwer <rahim.alwer@gmail.com>
2021-11-11 14:04:09 +1100

    fix(theme-default): prevent sidebar shifting up on short md pages

--- packages/theme-default/src/client/layout/Page.svelte
+++ packages/theme-default/src/client/layout/Page.svelte
@@ -31,7 +31,6 @@
     position: relative;
     margin: 0;
     width: 100%;
-    min-height: 100%;
   }
 
   .page[class*='md'] {
@@ -42,6 +41,7 @@
     display: flex;
     align-items: center;
     justify-content: center;
+    min-height: 100%;
   }
 
   .page__container {
