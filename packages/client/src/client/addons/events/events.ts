a3fbb2e7af987b44f7170ea7259223cc02c49981
Rahim Alwer <rahim.alwer@gmail.com>
2022-01-11 17:51:58 +1100

    feat: add `throttledEventCallback` for `EventsAddon`

--- packages/client/src/client/addons/events/events.ts
+++ packages/client/src/client/addons/events/events.ts
@@ -1,10 +1,12 @@
+import throttle from 'just-throttle';
 import { writable } from 'svelte/store';
 
 import { stringifyEvent } from './stringifyEvent';
 
 export const events = writable<Events>([]);
 
-export function eventCallback(event: EventEntryRef) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function eventCallback(event: any) {
   window.requestAnimationFrame(() => {
     events.update(($events) => {
       let stringifiedEvent;
@@ -20,6 +22,16 @@ export function eventCallback(event: EventEntryRef) {
       return $events.sort((a, b) => b.ref.timeStamp - a.ref.timeStamp);
     });
   });
+}
+
+export function throttledEventCallback(
+  interval = 0,
+  options?: {
+    leading?: boolean;
+    trailing?: boolean;
+  },
+) {
+  return throttle(eventCallback, interval, options);
 }
 
 export type EventEntry = {
