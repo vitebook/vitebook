9506a95f985fe237899796e4831df707475f2f0d
Rahim Alwer <rahim.alwer@gmail.com>
2022-01-31 00:20:27 +1100

    chore(preact): fix app file type from `component` to `Component`

--- packages/preact/src/virtual/app.d.ts
+++ packages/preact/src/virtual/app.d.ts
@@ -1,6 +1,6 @@
 declare module ':virtual/vitebook/preact/app' {
   import { FunctionComponent, ComponentChildren } from 'preact';
 
-  const App: FunctionComponent<{ component: ComponentChildren }>;
+  const App: FunctionComponent<{ Component: ComponentChildren }>;
   export default App;
 }
